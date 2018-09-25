577bf681e6bb2cabf6eadff912aa3d6944330ead
yingying <chen.yingying.01988@gmail.com>
2018-09-25 16:09:19 +0100

    test passed check stocking

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,10 +1,11 @@
 import java.util.ArrayList;
 
 public class Library {
-
+    private int capacity;
     ArrayList<Book> collection;
 
-    public Library(){
+    public Library(int capacity){
+        this.capacity = capacity;
         this.collection = new ArrayList<>();
     }
 
@@ -14,5 +15,13 @@ public class Library {
 
     public void addBookToCollection(Book book) {
         this.collection.add(book);
+    }
+
+    public boolean checkStock() {
+        if (this.collection.size()<= capacity){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -9,7 +9,7 @@ public class LibraryTest {
     
     @Before
     public void before(){
-        library = new Library();
+        library = new Library(5);
         book = new Book();
     }
     
@@ -29,5 +29,24 @@ public class LibraryTest {
         library.addBookToCollection(book);
         library.addBookToCollection(book);
         assertEquals(2, library.getNumberOfBook());
+    }
+
+    @Test
+    public void checkStockIsNotFull() {
+        library.addBookToCollection(book);
+        library.addBookToCollection(book);
+        assertEquals(true, library.checkStock());
+    }
+
+    @Test
+    public void checkStockIsFull() {
+        library.addBookToCollection(book);
+        library.addBookToCollection(book);
+        library.addBookToCollection(book);
+        library.addBookToCollection(book);
+        library.addBookToCollection(book);
+        library.addBookToCollection(book);
+        assertEquals(false, library.checkStock());
+
     }
 }
